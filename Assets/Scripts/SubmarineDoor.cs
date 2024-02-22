3b5711182c29de9dc93df8265b45f6622131cab0
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2024-02-22 14:38:54 +0100

    fixed door behavior

--- Assets/Scripts/SubmarineDoor.cs
+++ Assets/Scripts/SubmarineDoor.cs
@@ -9,7 +9,13 @@ public class SubmarineDoor : MonoBehaviour, IInteractuableObject
     [SerializeField] private Transform Submarine;
     public void OnInteract(MonoBehaviour playerThatInteracted)
     {
-        playerThatInteracted.gameObject.transform.position = InsidePosition.position;
+        Rigidbody rb = playerThatInteracted.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.Sleep();
+            rb.transform.position = InsidePosition.position;
+           
+        }
     }
 
     // Start is called before the first frame update
