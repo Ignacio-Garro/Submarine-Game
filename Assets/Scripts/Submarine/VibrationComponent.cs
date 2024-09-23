29a9dd26584116289d276225dbded5baa38e5e7b
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2024-09-23 15:02:17 +0200

    water pump

---
+++ Assets/Scripts/Submarine/VibrationComponent.cs
@@ -0,0 +1,43 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class VibrationComponent : NetworkBehaviour
+{
+
+    [SerializeField] float amplitud = 0.1f;
+    // Frecuencia de la vibración (controla qué tan rápido varía el ruido)
+    [SerializeField] float frecuencia = 1f;
+
+    // Posición original del objeto
+    private Vector3 posicionOriginal;
+
+    // Variables para el offset en Perlin Noise
+    private float offsetX;
+    private float offsetY;
+
+    bool isActive = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        posicionOriginal = transform.position;
+        offsetX = Random.Range(0f, 100f);
+        offsetY = Random.Range(0f, 100f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActive || !IsServer) return;
+        float desplazamientoX = (Mathf.PerlinNoise(Time.time * frecuencia + offsetX, 0) - 0.5f) * 2f * amplitud;
+        float desplazamientoY = (Mathf.PerlinNoise(0, Time.time * frecuencia + offsetY) - 0.5f) * 2f * amplitud;
+        transform.position = posicionOriginal + new Vector3(desplazamientoX, desplazamientoY, 0);
+    }
+
+    public void SetActiveVibration(bool active)
+    {
+        isActive = active;
+    }
+
+}
