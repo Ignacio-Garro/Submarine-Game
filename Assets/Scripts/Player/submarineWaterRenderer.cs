235c526712f2558b49f608c04f4711c1eef625da
Ignacio Garro <ignaciopapiro@gmail.com>
2024-10-05 15:26:37 +0200

    water

--- Assets/Scripts/Player/submarineWaterRenderer.cs
+++ Assets/Scripts/Player/submarineWaterRenderer.cs
@@ -1,36 +1,42 @@
 using UnityEngine;
 
-public class submarineWaterRenderer : MonoBehaviour
-{
+public class submarineWaterRenderer : MonoBehaviour{
     [SerializeField] private Camera camera;
-    [SerializeField] private GameObject insideWater;
-    [SerializeField] private GameObject outsideWater;
-    [SerializeField] private bool isInsideSubamrine = true;
+    [SerializeField] private bool isInsideSubmarine = true;
     private int insideWaterLayer;
     private int outsideWaterLayer;
 
     void Start()
     {
         // Get the layer of the target GameObject
-        insideWaterLayer = insideWater.layer;
-        outsideWaterLayer = outsideWater.layer;
+        insideWaterLayer = LayerMask.NameToLayer("insidePool");
+        outsideWaterLayer = LayerMask.NameToLayer("outsidePool");
 
-        camera = GetComponent<Camera>();
+        camera = GetComponentInChildren<Camera>();
     }
 
     void Update()
     {
-        if (isInsideSubamrine)
+        if (isInsideSubmarine)
         {
-            camera.cullingMask |= (1 << insideWaterLayer);
+            camera.cullingMask |= (1 << insideWaterLayer); //render
 
-            camera.cullingMask &= ~(1 << outsideWaterLayer);
+            camera.cullingMask &= ~(1 << outsideWaterLayer);//dont render
         }
         else
         {
-            camera.cullingMask &= ~(1 << insideWaterLayer);
+            camera.cullingMask &= ~(1 << insideWaterLayer);//dont render
 
-            camera.cullingMask |= (1 << outsideWaterLayer);
+            camera.cullingMask |= (1 << outsideWaterLayer);//render
         }
+    }
+
+    public void EnterSubmarine(){
+        isInsideSubmarine = true;
+        Debug.Log("enter submarine");
+    }
+    public void ExitSubmarine(){
+        isInsideSubmarine = false;
+        Debug.Log("exit submarine");
     }
 }
